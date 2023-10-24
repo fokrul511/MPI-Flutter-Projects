a45f256d190280da22981cc5fe6fc8c1f2cdf016
fokrul511 <islamfokrul2002@gmail.com>
2023-10-24 23:17:49 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-
       debugShowCheckedModeBanner: false,
       home: OnbordingScreen(),
     );
   }
 }
+//mpi apps
